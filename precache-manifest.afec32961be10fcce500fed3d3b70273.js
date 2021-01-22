4ca0cf80148167df0fd49bc8404ad033499331d0
mixermat99 <colamat99@gmail.com>
2021-01-22 14:52:16 +0100

    added file

---
+++ precache-manifest.afec32961be10fcce500fed3d3b70273.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "e43bb31b1f3d673b1999",
+    "url": "/static/js/app.07432e41.chunk.js"
+  },
+  {
+    "revision": "66090dc4495d4ce3dfb9",
+    "url": "/static/js/runtime~app.2e9f1821.js"
+  },
+  {
+    "revision": "c5d2c3f79c400eac37ac",
+    "url": "/static/js/2.739b3abb.chunk.js"
+  },
+  {
+    "revision": "b29f39bbc4563e85474cb78fba158e4f",
+    "url": "/static/media/login_sfondo.ceb57b6b.jpg"
+  },
+  {
+    "revision": "781faac613106c46ca7158ebf7af3226",
+    "url": "/static/media/login_logo.e6c72afa.png"
+  },
+  {
+    "revision": "a93ce644ba8278c665219eb6a66a2d89",
+    "url": "/static/media/logo-small.01630d1a.png"
+  },
+  {
+    "revision": "9ac398140a429aa68a9e45e74a4e0c55",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/serve.json"
+  },
+  {
+    "revision": "2002987acac9deb6803ab4ba381226ba",
+    "url": "/expo-service-worker.js"
+  },
+  {
+    "revision": "e802d2644326fd9fa701f1d969a8a94a",
+    "url": "/register-service-worker.js"
+  },
+  {
+    "revision": "adc2896548e468c3e1e88f2b79d96579",
+    "url": "/static/js/2.739b3abb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d324ddc823eca6c86688f62576c81e76",
+    "url": "/favicon-16.png"
+  },
+  {
+    "revision": "e72daee58e37c99d3553059eda418aa8",
+    "url": "/favicon-32.png"
+  },
+  {
+    "revision": "50b1bd72d8b0c569ffcad83d622ec176",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "ea5721aa1d590aa955e5a44682b2c494",
+    "url": "/manifest.json"
+  }
+];
